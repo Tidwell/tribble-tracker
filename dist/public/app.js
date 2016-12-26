82be18efe982c0e109a9884fb303c8a66ceebfe4
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2016-12-25 19:15:09 -0500

    build

--- dist/public/app.js
+++ dist/public/app.js
@@ -29,8 +29,23 @@
 					self.remove(self.scoreList.length - 1 - arrayIndex);
 				}
 			});
+
+			var data = localStorage.getItem('TRIBBLE_PLAYERS' + this.userId);
+			if (data) {
+				data = JSON.parse(data);
+			}
+			for (var prop in data) {
+				this[prop] = data[prop];
+			}
 		},
 		methods: {
+			persist: function persist() {
+				var data = JSON.stringify({
+					scoreTotal: this.scoreTotal,
+					scoreList: this.scoreList
+				});
+				localStorage.setItem('TRIBBLE_PLAYERS' + this.userId, data);
+			},
 			getUserClass: function getUserClass() {
 				var obj = {};
 				obj['user' + this.userId] = true;
@@ -39,10 +54,12 @@
 			add: function add(num) {
 				this.scoreTotal = this.scoreTotal + num;
 				this.scoreList.push(this.scoreTotal);
+				this.persist();
 			},
 			subtract: function subtract(num) {
 				this.scoreTotal = this.scoreTotal - num;
 				this.scoreList.push(this.scoreTotal);
+				this.persist();
 			},
 			addMany: function addMany() {
 				var num = prompt('How many to add?');
@@ -62,6 +79,7 @@
 					this.scoreTotal = Number(num);
 					this.scoreList.push(Number(num));
 				}
+				this.persist();
 			},
 			remove: function remove(arrayIndex) {
 				this.scoreList.splice(arrayIndex, 1);
@@ -70,6 +88,7 @@
 				} else {
 					this.scoreTotal = 0;
 				}
+				this.persist();
 			}
 		},
 		computed: {
@@ -85,23 +104,48 @@
 			players: [],
 			round: 1
 		},
+		created: function created() {
+			var data = localStorage.getItem('TRIBBLE_APP');
+			if (data) {
+				data = JSON.parse(data);
+			}
+			for (var prop in data) {
+				this[prop] = data[prop];
+			}
+		},
 		methods: {
+			persist: function persist() {
+				var data = JSON.stringify({
+					players: this.players,
+					round: this.round
+				});
+				localStorage.setItem('TRIBBLE_APP', data);
+			},
 			addPlayer: function addPlayer() {
 				var name = prompt('Player name?');
 				if (name) {
 					this.players.push(name);
 				}
+				this.persist();
 			},
 			addRound: function addRound() {
 				this.round++;
 				if (this.round > 5) {
 					this.round = 1;
 				}
+				this.persist();
 			},
 			reset: function reset() {
 				if (confirm('Are You Sure?')) {
 					this.players = [];
 					this.round = 1;
+
+					var i = 0;
+					while (i < 6) {
+						localStorage.removeItem('TRIBBLE_PLAYERS' + i);
+						i++;
+					}
+					this.persist();
 				}
 			}
 		},
@@ -123,3 +167,80 @@
 		}
 	});
 })();
+
+if (!window.localStorage) {
+	Object.defineProperty(window, "localStorage", new function () {
+		var aKeys = [],
+		    oStorage = {};
+		Object.defineProperty(oStorage, "getItem", {
+			value: function value(sKey) {
+				return sKey ? this[sKey] : null;
+			},
+			writable: false,
+			configurable: false,
+			enumerable: false
+		});
+		Object.defineProperty(oStorage, "key", {
+			value: function value(nKeyId) {
+				return aKeys[nKeyId];
+			},
+			writable: false,
+			configurable: false,
+			enumerable: false
+		});
+		Object.defineProperty(oStorage, "setItem", {
+			value: function value(sKey, sValue) {
+				if (!sKey) {
+					return;
+				}
+				document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+			},
+			writable: false,
+			configurable: false,
+			enumerable: false
+		});
+		Object.defineProperty(oStorage, "length", {
+			get: function get() {
+				return aKeys.length;
+			},
+			configurable: false,
+			enumerable: false
+		});
+		Object.defineProperty(oStorage, "removeItem", {
+			value: function value(sKey) {
+				if (!sKey) {
+					return;
+				}
+				document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+			},
+			writable: false,
+			configurable: false,
+			enumerable: false
+		});
+		this.get = function () {
+			var iThisIndx;
+			for (var sKey in oStorage) {
+				iThisIndx = aKeys.indexOf(sKey);
+				if (iThisIndx === -1) {
+					oStorage.setItem(sKey, oStorage[sKey]);
+				} else {
+					aKeys.splice(iThisIndx, 1);
+				}
+				delete oStorage[sKey];
+			}
+			for (aKeys; aKeys.length > 0; aKeys.splice(0, 1)) {
+				oStorage.removeItem(aKeys[0]);
+			}
+			for (var aCouple, iKey, nIdx = 0, aCouples = document.cookie.split(/\s*;\s*/); nIdx < aCouples.length; nIdx++) {
+				aCouple = aCouples[nIdx].split(/\s*=\s*/);
+				if (aCouple.length > 1) {
+					oStorage[iKey = unescape(aCouple[0])] = unescape(aCouple[1]);
+					aKeys.push(iKey);
+				}
+			}
+			return oStorage;
+		};
+		this.configurable = false;
+		this.enumerable = true;
+	}());
+}
